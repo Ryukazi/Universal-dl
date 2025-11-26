e75ad92b972e6c2bdc14440a0e6e58fe25558806
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2025-11-26 14:43:33 +0545

    Fix duplicate URL in axios request

--- services/tiktokService.js
+++ services/tiktokService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const download = async (url) => {
-  const { data } = await axios.get(`https://https://ttt-pprj.onrender.com/api/tiktok?url=${encodeURIComponent(url)}`);
+  const { data } = await axios.get(`https://ttt-pprj.onrender.com/api/tiktok?url=${encodeURIComponent(url)}`);
   return data;
 };
