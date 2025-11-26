d49416558372560e1fb28c1712585d04d4d30af9
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2025-11-26 17:27:03 +0545

    Add Spotify download service using axios

---
+++ services/spotifyService.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const download = async (url) => {
+  const { data } = await axios.get(`https://prenivdl.vercel.app/api/download?url=${encodeURIComponent(url)}`);
+  return data;
+};
