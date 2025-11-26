196a15055642832d0ba731175ef9c0ffb652f899
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2025-11-26 18:37:00 +0545

    Rename download function to downloadSpotify

--- services/spotifyService.js
+++ services/spotifyService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const download = async (url) => {
-  const { data } = await axios.get(`https://prenivdl.vercel.app/api/download?url=${encodeURIComponent(url)}`);
+export const downloadSpotify = async (url) => {
+  const { data } = await axios.get(
+    `https://prenivdl.vercel.app/api/download?url=${encodeURIComponent(url)}`
+  );
   return data;
 };
