68b73f3b1e50ae525cadb2cf0f913538afc4cb81
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2025-12-01 18:02:23 +0545

    Update tiktokService.js

--- services/tiktokService.js
+++ services/tiktokService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const download = async (url) => {
-  const { data } = await axios.get(`https://ttt-pprj.onrender.com/api/tiktok?url=${encodeURIComponent(url)}`);
+  const { data } = await axios.get(`https://ttt-pprj.onrender.com/api/download?url=${encodeURIComponent(url)}`);
   return data;
 };
